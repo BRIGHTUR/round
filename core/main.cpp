b8804ea3eac4d497c2e1509401a001a3bc1f54c8
BRIGHTUR <50517987+BRIGHTUR@users.noreply.github.com>
2020-04-17 16:57:44 +0800

    颜色

--- core/main.cpp
+++ core/main.cpp
@@ -4,15 +4,17 @@ using namespace std;
 using namespace cv;
 int main()
 {
-	Mat src = imread("die_on_chip.png", 0);
+	Mat src = imread("die_on_chip.png");
 	Mat dist;
+	Mat dist1;
 	Mat proc;
 	Mat proc1;
 	Mat labelmat;
 	Mat statsmat;
 	Mat centermat;
 	Mat dstImage;
-	threshold(src, dist, 150, 255, THRESH_BINARY);
+	cvtColor(src, dist1, CV_BGR2GRAY);
+	threshold(dist1, dist, 150, 255, THRESH_BINARY);
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 	findContours(dist, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
@@ -22,11 +24,12 @@ int main()
 		float width = rbox.size.width;
 		float height = rbox.size.height;
 		float test = width / height;
-		cout << test << endl;
+		//cout << test << endl;
 		if (test>0.9&&test<1.1) {
-			drawContours(src, contours, i, Scalar(255, 255, 0), 1, 8);
+			drawContours(src, contours, i, Scalar(255, 44, 33), 1, 8);
 			Point2f vtx[4];
 			rbox.points(vtx);
+			cout << test << endl;
 			for (int j = 0; j < 4; ++j) {
 				line(src, vtx[j], vtx[j < 3 ? j + 1 : 0], Scalar(33, 44, 255), 2, CV_AA);
 			}
